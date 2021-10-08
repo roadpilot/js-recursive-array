782773406ca2694ab01b84656ae1e5c5fd3fdab5
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-08 13:23:01 +0000

    test non-recursive

--- index.js
+++ index.js
@@ -5,8 +5,10 @@
 
 function capitalizeFirst(arr){
   for (i=0; i<arr.length; i++){
-    console.log(arr[i])
+    arr[i]=arr[i][0].toUpperCase()+arr[i].slice(1)
   }
+  console.log(arr)
 }
+
 
 capitalizeFirst(['car','taco','banana'])
