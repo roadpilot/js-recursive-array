10c559a0b62788a662df5857f72dd13cf1efe545
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-08 13:30:02 +0000

    cleanup

--- index.js
+++ index.js
@@ -4,12 +4,9 @@
  */
 
 function capitalizeFirst(arr, i=0){
-  // for (i=0; i<arr.length; i++){
-    if (i>arr.length-1){return arr}
-    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1)
-  // }
+  if (i>arr.length-1){return arr}
+  arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1)
   return (capitalizeFirst(arr,i+1))
 }
-
 
 capitalizeFirst(['car','taco','banana'])
